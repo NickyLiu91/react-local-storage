a0db42879696d39c81880e3aed9a57b39603c38a
Nicky <nickyliu91@gmail.com>
2020-04-16 21:22:22 -0400

    basic form structure set

--- src/home.js
+++ src/home.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-const Home = (props) => {
-  return(
-    <div>
-      ????????
-    </div>
-  )
+class Home extends React.Component {
+  render() {
+    return(
+      <div>
+      <form>
+      Name <input type="text"/>
+      </form>
+      <form>
+      Password <input type="text"/>
+      </form>
+      </div>
+    )
+  }
 }
 
 export default Home;
