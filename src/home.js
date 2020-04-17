440c3576dc9fc1ed3eab798ab40039a5481fc9e1
Nicky <nickyliu91@gmail.com>
2020-04-16 21:30:01 -0400

    set up state

--- src/home.js
+++ src/home.js
@@ -1,15 +1,22 @@
 import React from "react";
 
 class Home extends React.Component {
+
+  state = {
+    name: '',
+    password: ''
+  }
+
   render() {
     return(
       <div>
-      <form>
-      Name <input type="text"/>
-      </form>
-      <form>
-      Password <input type="text"/>
-      </form>
+        <form>
+        Name <input type="text"/>
+        </form>
+        <form>
+        Password <input type="text"/>
+        </form>
+        {this.state.name}
       </div>
     )
   }
